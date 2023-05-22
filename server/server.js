882588c0c04d134c933bea435bdd19bce30d8f0e
danushanavod <danushanavod@outlook.com>
2023-05-22 09:39:57 +0530

    Node setup done

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+// import records from "./routes/record.mjs";
+import * as dotenv from 'dotenv';
+dotenv.config()
+
+console.log(process.env)
+
+const PORT = process.env.PORT || 5050;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// app.use("/record", records);
+
+// start the Express server
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
